6de93c4985644a15d05d3c7197187b43de090c96
Masato Hagiwara <masato@duolingo.com>
2016-02-06 07:51:35 -0500

    Join HSK word list with CC-CEDICT.

---
+++ hsk/join_with_cedict.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+import sys
+from collections import defaultdict
+import json
+
+
+def read_cedict(cedict_file):
+    """Read the CC-CEDICT file and returns a dict from word surface form to set of entries."""
+    cedict = defaultdict(set)
+    for line in cedict_file:
+        line = line.strip()
+        json_dict = json.loads(line)
+        cedict[json_dict['word_sp']].add(line)
+        cedict[json_dict['word_tr']].add(line)
+
+    return cedict
+
+
+def main(ifile=sys.stdin, ofile=sys.stdout, cedict_file=None):
+    assert cedict_file
+
+    cedict = read_cedict(cedict_file)
+
+    for line in ifile:
+        word, pos, hsk_level = line.decode('utf-8').strip().split('\t')
+        cedict_entries = [e for e in cedict[word]
+                          if 'variant' not in e and 'surname' not in e]
+        for entry in cedict_entries:
+            ofile.write(u'%s\t%s\t%s\t%s\n' % (word, pos or '', hsk_level, entry))
+
+if __name__ == '__main__':
+    import codecs
+    main(ofile=codecs.getwriter('utf_8')(sys.stdout),
+         cedict_file=codecs.open(sys.argv[1], encoding='utf-8'))
